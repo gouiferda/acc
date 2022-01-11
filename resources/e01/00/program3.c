f3ea908ae928150d316070dc631596f31920ef51
space <soufiane.gouiferda@gmail.com>
2022-01-11 16:38:02 +0100

    ok

--- resources/e01/00/program3.c
+++ resources/e01/00/program3.c
@@ -20,19 +20,43 @@ void fonction_1(){
 
 // [type de valeur returné] [nom de fonction] (variable : parmeters){}
 void calculer_age(int annee_naissance,int mois){
-    
-    int age_mois = ((2022 - annee_naissance) * 12) - mois;
-    
-    float age = age_mois / 12;
-    
-    printf("Age : %.2f\n",age);
+   
+    int age_annee_simple = (2022 - annee_naissance); //  2022 - 1997 = 25
+    int age_mois_simple = (age_annee_simple * 12); //25 * 12 = 300 mois
+    float age_mois_exact = age_mois_simple - mois; // 296
+    double age = (double) (age_mois_exact / 12);
+
+
+    printf("Age : %1f\n",age); //24.6
+}
+
+
+void calculAge_2(int anneeNaiss, int moisNaiss){
+	int thisMonth = 1;
+	int thisYear = 2022;
+	int ageYear = 0;
+	int ageMonth = 0;
+	if(moisNaiss > thisMonth){
+		ageYear = thisYear - anneeNaiss - 1;
+		ageMonth = 12 - moisNaiss + thisMonth;
+	}
+	if(moisNaiss < thisMonth){
+		ageYear = thisYear - anneeNaiss;
+		ageMonth = thisMonth - moisNaiss;
+	}
+	if(moisNaiss == thisMonth){
+		ageYear = thisYear - anneeNaiss;
+		ageMonth = 0;
+	}
+	printf("Vous avez %d annees et %d mois",ageYear, ageMonth);
 }
 
 int main(){
 
     // printf("sndo9_1 : %d\n",sndok_1);
     // fonction_1();  //calling function_1
-    calculer_age(1994,12); //calling calculer_age
+   // calculer_age(1999,11); //calling calculer_age
+   // calculAge_2(1994,12);
 
     return (0);
 }
